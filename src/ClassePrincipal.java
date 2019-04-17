ea923c83189af971f8535abe9e46408572ef1560
Raphael Branco <raphael.pieroni@aluno.soueinstein.com.br>
2019-04-17 20:28:35 -0300

    First commit

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -6,53 +6,51 @@ public class ClassePrincipal {
     
     public static void main(String [] args) {
         
-       Carro carro = new Carro();
+       Carro carro01 = new Carro();
        
-       carro.nome = "Fusca";
-       carro.marca = "Volkswagem";
-       carro.cor = "Azul";
-       carro.fabricacao = "1556";
-       carro.largura = "1,70 mm";
-       carro.peso = "1300KG";
+       Scanner scan = new Scanner (System.in)  
+              
+       System.out.print("Nome: ");
+       carro01.nome = scan.next();
+       System.out.print("Marca: ");
+       carro01.marca = scan.next();
+       System.out.print("cor: ");
+       carro01.cor = scan.next();
+       System.out.print("Fabricacao: ");
+       carro01.fabricacao = scan.next();
+       System.out.print("Largura: ");
+       carro01.largura = scan.next();
+       System.out.print("Peso: ");
+       carro01.peso = scan.next();
        
-       System.out.println ("Nome:" + carro.nome);
-       System.out.println ("Marca:" + carro.marca);
-       System.out.println ("Cor:" + carro.cor);
-       System.out.println ("Fabricacao:" + carro.fabricacao);
-       System.out.println ("Largura:" + carro.largura);
-       System.out.println ("Peso:" + carro.peso);
+       System.out.println ("Nome:" + carro01.nome);
+       System.out.println ("Marca:" + carro01.marca);
+       System.out.println ("Cor:" + carro01.cor);
+       System.out.println ("Fabricacao:" + carro01.fabricacao);
+       System.out.println ("Largura:" + carro01.largura);
+       System.out.println ("Peso:" + carro01.peso);
     
-       Carro brasilia = new Carro () ;
-    
-     brasilia.nome = "brasilia";
-       brasilia.marca = "Volkswagem";
-       brasilia.cor = "Azul";
-       brasilia.fabricacao = "1973";
-       brasilia.largura = "1,50 mm";
-       brasilia.peso = "900KG";
-      
-     System.out.println ("Nome:" + brasilia.nome);
-     System.out.println ("Marca:" + brasilia.marca);
-     System.out.println ("Cor:" + brasilia.cor);
-     System.out.println ("Fabricacao:" + brasilia.fabricacao);
-     System.out.println ("Largura:" + brasilia.largura);
-     System.out.println ("Peso:" + brasilia.peso);
-        Carro carro01 = new Carro ();
-        
-            Scanner scan1 = new Scanner (System.in) ;
-                System.out.print("Nome: ");
-                carro01.nome = scan1.next();
-                System.out.print("Marca: ");
-                     carro01.marca = scan1.next();
-                System.out.print("cor: ");
-                carro01.cor = scan1.next();
-                System.out.print("Fabricacao: ");
-                carro01.fabricacao = scan1.next();
-                System.out.print("Largura: ");
-                carro01.largura = scan1.next();
-                System.out.print("Peso: ");
-                carro01.peso = scan1.next();
-
+       Carro carro02 = new Carro () ;
+       System.out.print("Nome: ");
+       carro02.nome = scan.next();
+       System.out.print("Marca: ");
+       carro02.marca = scan.next();
+       System.out.print("cor: ");
+       carro02.cor = scan.next();
+       System.out.print("Fabricacao: ");
+       carro02.fabricacao = scan.next();
+       System.out.print("Largura: ");
+       carro02.largura = scan.next();
+       System.out.print("Peso: ");
+       carro02.peso = scan.next();
+     
+     System.out.println ("Nome:" + carro02.nome);
+     System.out.println ("Marca:" + carro02.marca);
+     System.out.println ("Cor:" + carro02.cor);
+     System.out.println ("Fabricacao:" + carro02.fabricacao);
+     System.out.println ("Largura:" + carro02.largura);
+     System.out.println ("Peso:" + carro02.peso);
+     
 
 
      
